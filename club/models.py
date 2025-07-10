46fbb25fff91c1929186dc9705b0550bc3875348
hotbari <138793425+hotbari@users.noreply.github.com>
2025-07-10 15:42:23 +0900

    Change ForeignKey on_delete behavior to SET_NULL to preserve data 기존 DO_NOTHING 설정으로 인해 참조된 객체 삭제 시 무결성 오류 발생. SET_NULL로 변경하여 참조 객체 삭제 시 해당 필드를 NULL로 처리, 데이터 보존 가능.

--- club/models.py
+++ club/models.py
@@ -3,13 +3,12 @@ from core.models import TimeStampedModel, SoftDeleteModel
 
 
 class Club(TimeStampedModel, SoftDeleteModel):
-    id = models.AutoField(primary_key=True)
     address = models.CharField(max_length=100, blank=True, null=True)
     phone = models.CharField(max_length=30, blank=True, null=True)
     name = models.CharField(max_length=30, blank=True, null=True)
     description = models.CharField(max_length=100, blank=True, null=True)
     image_url = models.ForeignKey(
-        'image_url.ImageUrl', on_delete=models.DO_NOTHING, blank=True, null=True)
+        'image_url.ImageUrl', on_delete=models.SET_NULL, blank=True, null=True)
     objects = models.Manager()
 
     def __str__(self):
